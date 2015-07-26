121a26fc866a185cafaaffc9862fa57ff1d72cc2
houzy <h-zy2008@hotmail.com>
2015-07-26 22:02:34 +0800

    Update GetSenscapeVersionNum.java

--- src/getLatestVersionNum/GetSenscapeVersionNum.java
+++ src/getLatestVersionNum/GetSenscapeVersionNum.java
@@ -1,11 +1,16 @@
 package getLatestVersionNum;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import retrofit.RestAdapter;
 import retrofit.http.GET;
 import retrofit.http.Query;
 import rx.Observable;
+import rx.Scheduler;
 import rx.Subscriber;
 import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 public class GetSenscapeVersionNum {
 
@@ -55,10 +60,17 @@ public class GetSenscapeVersionNum {
 
 	private void getLatestVersion() {
 		Observable<VersionResponse> versionResponseObservable = getLatestVersionNum(0);
-
-		versionResponseObservable
-			//.subscribeOn(Schedulers.newThread())
-			//.observeOn(Schedulers.mainThread())
+		System.out.println("GetLatestVersionNum");
+		
+		ExecutorService service = Executors.newSingleThreadExecutor();
+		Scheduler scheduler     = Schedulers.from(service);
+		
+		// http://stackoverflow.com/questions/20451939/observeon-and-subscribeon-where-the-work-is-being-done
+		// http://stackoverflow.com/questions/7579237/whats-the-difference-between-subscribeon-and-observeon
+		versionResponseObservable //注意观察main before sleep的打印时间，如果都不指定，所有执行完了才运行main
+			//.subscribeOn(Schedulers.newThread()) //运行.subscribe返回的handle
+			//.subscribeOn(scheduler) // 此方法用于指定一个特定的线程，newSingleThreadExecutor得到的线程main线程退出后还不退出
+			//.observeOn(Schedulers.newThread()) //用于运行OnNext, OnCompleted & OnError等。
 			.subscribe(
 					new Action1<VersionResponse>() {
 						@Override
@@ -66,7 +78,7 @@ public class GetSenscapeVersionNum {
 							System.out.println("--------versionResponse.getStatus()-----" + versionResponse.getStatus());
 							System.out.println("--------versionResponse.getNotice()-----" + versionResponse.getNotice());
 							System.out.println("--------versionResponse.getData()-----" + versionResponse.getData());
-
+							System.out.println("Running on: " + Thread.currentThread().getId());
 						}
 					},
 					new Action1<Throwable>() {
@@ -74,6 +86,7 @@ public class GetSenscapeVersionNum {
 						public void call(Throwable e) {
 							System.out.println("onError");
 							e.printStackTrace();
+							System.out.println("Running on: " + Thread.currentThread().getId());
 						}
 					});
 	}
@@ -83,11 +96,23 @@ public class GetSenscapeVersionNum {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("GetLatestVersionNum");
+		System.out.println("Main entry");
 
+		System.out.println("Running on: " + Thread.currentThread().getId());
+				
 		GetSenscapeVersionNum getSenscapeVersionNum = new GetSenscapeVersionNum();
 		getSenscapeVersionNum.getLatestVersion();
-
+		
+		// 当此处没有添加延时时，使用.subscribeOn(Schedulers.newThread())
+		// 会造成订阅者没有执行，原因是主线程已经退出了，其他线程也一起终结了。
+		System.out.println("main before sleep");
+		try {
+			Thread.currentThread().sleep(3000); // 3000ms
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("main finished");
 	}
 
 }
